f22147289076c368eebc4ee544d59e5e3e15124a
Abhinav Ojha <abhinavojha@gofynd.com>
2023-09-15 15:29:45 +0530

    initial commit

---
+++ src/main/java/com/kunal_kushvaha/Java_11_Basic/Question2.java
@@ -0,0 +1,14 @@
+package com.kunal_kushvaha.Java_11_Basic;
+
+
+import java.util.Scanner;
+
+public class Question2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println("Hi "+input);
+
+	}
+
+}
---
+++ src/main/java/com/kunal_kushvaha/Java_14_BinarySearch/Test.java
@@ -0,0 +1,8 @@
+package com.kunal_kushvaha.Java_14_BinarySearch;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Main");
+	}
+
+}
